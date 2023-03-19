f4fbd2ddee4f95384b2cadf045fc85f49a4b8069
Klaus Ma <klausm@nvidia.com>
2023-03-19 04:56:12 +0200

    list pci for now.
    Signed-off-by: Klaus Ma <klausm@nvidia.com>

--- libhca/src/lib.rs
+++ libhca/src/lib.rs
@@ -25,6 +25,7 @@ mod pci;
 pub struct HCA {
     pub description: String,
     pub serial_number: String,
+    pub vendor_id: u32,
     pub driver: String,
     pub functions: Vec<Function>,
 }
@@ -35,16 +36,52 @@ pub struct Function {
     pub lid: i32,
 }
 
+pub struct PciDevice {
+    pub vendor_id: u32,
+    pub device_id: u32,
+
+}
+
+pub fn list_pci_device() -> Vec<PciDevice> {
+    let mut pcidev = vec![];
+
+    unsafe {
+        let pacc = pci::pci_alloc();
+        pci::pci_init(pacc);
+
+        pci::pci_scan_bus(pacc);
+
+        let mut dev = (*pacc).devices;
+
+        loop {
+            if dev.is_null() {
+                break;
+            }
+
+            pcidev.push( PciDevice{
+                    vendor_id: (*dev).vendor_id as u32,
+                    device_id: (*dev).device_id as u32,
+            });
+
+            println!("{:<15}: 0x{:x}", "Vendor ID", (*dev).vendor_id);
+            println!("{:<15}: 0x{:x}", "Device ID", (*dev).device_id);
+            println!("{:<15}: 0x{:x}", "Device Class", (*dev).device_class);
+
+            dev = (*dev).next
+        }
+
+        pci::pci_cleanup(pacc);
+    };
+
+
+    pcidev
+}
+
 /// List the HCAs on the host.
 pub fn list_hca() -> Vec<HCA> {
     let mut hcas = vec![];
 
-    //     unsafe {
-    //         let hca_list = pci::get_hca_list();
-    //
-    //
-    //         pci::free_hca_list(hca_list);
-    //     }
+    let _pcidev = list_pci_device();
 
     hcas
 }
